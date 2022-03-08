a4d2fe00a2237d4d869226fd9563d9f2eb4c0735
Auzan <auzan.ip@gmail.com>
2022-03-08 10:28:25 +0700

    first commit

---
+++ src/layouts/PageLayout/index.js
@@ -0,0 +1,42 @@
+
+//NOT EDITABLE!!!
+import Topography from "../../components/Topography"
+import Hero from "../../components/Hero"
+import Footer from "../../components/Footer"
+
+// EDITABLE
+import background from "../../assets/img/background.jpg" //Background
+import logo from '../../assets/img/logo.png'; //Logo
+
+
+function pageLayout( props ) {
+    //EDITABLE
+    const deskripsi = "Selamat datang kembali di"
+    const title1 = "Tangerang Selatan"
+    const title2 = "Blended Learning"
+    const slogan = "Belajar tanpa berpikir itu tidaklah berguna, tapi berpikir tanpa belajar itu sangatlah berbahaya."
+    const copyright = "2021 Tangerang Selatan Blended Learning - Didukung oleh Geschool"
+
+    return (
+        <div>
+            <Topography />
+            <Hero 
+                logo={logo}
+                background={background}
+                deskripsi={deskripsi}
+                title1={title1}
+                title2={title2}
+                slogan={slogan}
+                appName={props.appName}
+            >
+                { props.children }
+            </Hero>
+            <Footer
+                copyright={copyright}
+            />
+        </div>
+    );
+}
+
+
+export default pageLayout;
